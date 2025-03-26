4b305448a3b1363af1e8734f5997ef703061c026
Wilfried Essomba <pharaonw15@gmail.com>
2025-03-26 11:31:03 +0100

    Some updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
   <div class="home text-center" id="home">
     <h1 class=" my-5">HELLO, I'M ESSOMBA WILFRIED</h1>
     <p class=" my-5">
-      A Result-Oriented Fullstack Developer building and managing Mobile/Web Apps and <br> Web Applications
+      A Result-Oriented Fullstack Developer building and managing Mobile Application and <br> Web Applications
       that leads to the success of the overall product.
     </p>
     <a class="project-btn btn btn-primary py-2 px-4" href="#project">See my Projects</a>
